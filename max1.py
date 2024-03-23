99a338d40191b32df1c18e36044512b8f175fb91
yupirupi <154742976+yupirupi@users.noreply.github.com>
2024-03-23 20:48:18 +0200

    Create max1.py

---
+++ max1.py
@@ -0,0 +1,16 @@
+import sqlite3
+import json
+
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+
+cursor.execute('select max(price) from goods')
+x = cursor.fetchall()
+x = json.dumps(x[0][0])
+
+
+print('Content-type: text/html\n')
+
+print(x)
+
